33ffb8340b5710773fbfb8c98bb056d993040f04
Krisha Ranawat <117197176+krisharanawat@users.noreply.github.com>
2023-07-21 21:24:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+
+var numberOfButtons = document.querySelectorAll(".drum").length;
+
+for(var i=0 ; i< numberOfButtons ; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    
+        //this.style.color = "white"
+        var buttonInnerHTML = this.innerHTML;
+        var audio;
+        switch(buttonInnerHTML){
+            case "w":
+                audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+            case "a":
+                audio = new Audio("sounds/tom-2.mp3");
+                audio.play();
+                break;
+            case "s":
+                audio = new Audio("sounds/tom-3.mp3");
+                audio.play();
+                break;
+            case "d":
+                audio = new Audio("sounds/tom-4.mp3");
+                audio.play();
+                break;
+            case "j":
+                audio = new Audio("sounds/snare.mp3");
+                audio.play();
+                break;
+            case "k":
+                audio = new Audio("sounds/crash.mp3");
+                audio.play();
+                break;
+            case "l":
+                audio = new Audio("sounds/kick.mp3");
+                audio.play();
+                break;
+            default:
+                console.log(buttonInnerHTML);
+            }
+        })
+        
+    };
